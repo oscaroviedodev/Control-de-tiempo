ae1b2cd215c6b3556e1f443b5dfa2e2250be134d
oscar <oscaroviedodev@outlook.com>
2026-01-24 18:04:18 -0500

    feat: definir logica de la lamina del reloj

--- src/LaminaReloj.java
+++ src/LaminaReloj.java
@@ -1,11 +1,35 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+import java.time.LocalTime;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class LaminaReloj extends JPanel{
     
+    private final Timer timer;
+    private final Reloj reloj = new Reloj();
+    private final JLabel labelReloj = new JLabel();    
+    
     public LaminaReloj() {
-        this.add(new JLabel("lamina reloj"));
-                
+        this.setLayout(new GridBagLayout());
+        timer = new Timer(1000, reloj);
+        timer.setInitialDelay(0);
+        timer.start();
+        this.add(labelReloj);
+    }
+    
+    class Reloj implements ActionListener{        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            LocalTime horaEnVivo = LocalTime.now();   
+            int hora = horaEnVivo.getHour();
+            int minuto = horaEnVivo.getMinute();
+            int segundo = horaEnVivo.getSecond();
+            String horaFormateada = String.format("%02d:%02d:%02d", hora, minuto, segundo);
+            labelReloj.setText(horaFormateada);
+        }
     }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Marco miMarco = new Marco();
-        LaminaContenedora l = new LaminaContenedora();
+        Marco miMarco = new Marco();       
         
     }
 }
